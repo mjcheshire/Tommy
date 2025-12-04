69dc656963ed2787c53acd32032c1668052c3155
MattC <MattC@192.168.1.211>
2025-12-04 17:14:40 +0000

    First boy boy

---
+++ Tommy/src/Boyboy.java
@@ -0,0 +1,12 @@
+
+public class Boyboy {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int zzz = 34;
+		System.out.println("Hello, World");
+		System.out.flush();
+		System.out.println("xxxxx" + zzz);
+	}
+}
