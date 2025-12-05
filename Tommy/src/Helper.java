d71408630ccd9fa560a0af4e1bd509625b9dc82c
mjcheshire <m.cheshire@yahoo.co.uk>
2025-12-05 12:48:39 +0000

    change in a brach

--- Tommy/src/Helper.java
+++ Tommy/src/Helper.java
@@ -4,7 +4,7 @@ public class Helper {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int zzz = 34;
-		System.out.println("Hello, WorldMMM");
+		System.out.println("Hello, WorldHHH");
 		System.out.flush();
 		System.out.println("xxxxx" + zzz);
 	}
