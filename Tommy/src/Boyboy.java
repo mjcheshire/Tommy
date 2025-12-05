5f30797b8dcf568a29fff3a48b9cdb8bb685a747
mjcheshire <m.cheshire@yahoo.co.uk>
2025-12-05 12:40:45 +0000

    QQQQQ

--- Tommy/src/Boyboy.java
+++ Tommy/src/Boyboy.java
@@ -5,7 +5,7 @@ public class Boyboy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int zzz = 34;
-		System.out.println("Hello, WorldOOO");
+		System.out.println("Hello, WorldQQQ");
 		System.out.flush();
 		System.out.println("xxxxx" + zzz);
 	}
