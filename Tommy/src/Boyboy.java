974bc71a179ca4b2177d0813d6b9a56934ebc782
mjcheshire <m.cheshire@yahoo.co.uk>
2025-12-09 16:22:32 +0000

    Update Boyboy.java
    helper comment

--- Tommy/src/Boyboy.java
+++ Tommy/src/Boyboy.java
@@ -5,7 +5,7 @@ public class Boyboy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int zzz = 34;
-		System.out.println("Hello, WorldMASTER");
+		System.out.println("Hello, WorldHELPER");
 		System.out.flush();
 		System.out.println("xxxxx" + zzz);
 	}
